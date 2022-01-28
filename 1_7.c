593eeb4919f591c0dd06fe04b8741da17ad566ef
AlexandrAndrieiev <98545372+AlexandrAndrieiev@users.noreply.github.com>
2022-01-28 13:33:01 +0200

    Андрєєв Classwork2, task 1_7

---
+++ 1_7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float x;
+	printf("x=");
+	scanf_s("%f", &x);
+	int y;
+	y = (int)x;
+	float y1;
+	y1 = x - y;
+	printf("%d\n", y);
+	printf("%f", y1);
+}
