e103eb522ce0b856c518a692f1c263b0d716fbd0
AlexandrAndrieiev <98545372+AlexandrAndrieiev@users.noreply.github.com>
2022-02-23 20:58:50 +0200

    HW2

---
+++ 1_21.c
@@ -0,0 +1,10 @@
+/*Task 1.21*/
+#include<stdio.h>
+int main()
+{
+	float a;
+	scanf_s("%f", &a);
+	printf("- | - | %f | - | -\n",a);
+	printf("- | %f | %f | %f | -\n", a,a,a);
+	printf("%f | %f | %f | %f | %f", a,a,a,a,a);
+}
